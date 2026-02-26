6ed89f88d87a4c820227a9f88abbe3e87e6626cc
Mark Garner <mgarner@microsoftcom>
2026-02-26 15:02:11 -0600

    [builder] Add AuditionDate entity to Domain layer

---
+++ src/Stretto.Domain/Entities/AuditionDate.cs
@@ -0,0 +1,11 @@
+namespace Stretto.Domain.Entities;
+
+public class AuditionDate
+{
+    public Guid Id { get; set; }
+    public Guid ProgramYearId { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public int BlockLengthMinutes { get; set; }
+    public Guid OrganizationId { get; set; }
+}
