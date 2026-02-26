86622192e436e9fd6ac09473001818ebb849b162
Mark Garner <mgarner@microsoftcom>
2026-02-26 17:26:10 -0600

    [builder] Register INotificationService in DI; fix merge conflicts in audition controllers and middleware

--- src/Stretto.Api/Program.cs
+++ src/Stretto.Api/Program.cs
@@ -27,6 +27,7 @@ builder.Services.AddScoped<IAuditionService, AuditionService>();
 builder.Services.AddScoped<IAttendanceService, AttendanceService>();
 builder.Services.AddScoped<IStorageProvider, LocalFileStorageProvider>();
 builder.Services.AddScoped<INotificationProvider, LogNotificationProvider>();
+builder.Services.AddScoped<INotificationService, NotificationService>();
 builder.Services.AddScoped<IProjectMaterialsService, ProjectMaterialsService>();
 builder.Services.AddScoped<IDashboardService, DashboardService>();
 
